860f4c98eca3d7db76ac7d9cf12898bc51a2513b
kusssomax <maxkusjo@gmail.com>
2025-11-12 23:08:46 +0200

    new videos

---
+++ src/components/VideoSection/constants.ts
@@ -0,0 +1,9 @@
+import video1 from "@/assets/videos/video1_new.mp4";
+import video2 from "@/assets/videos/video2_new.mp4";
+import video3 from "@/assets/videos/trial_video_new.mp4";
+import video4 from "@/assets/videos/video4.mp4";
+import video5 from "@/assets/videos/video5.mp4";
+
+
+export const videos = [video4, video5, video1, video2, video3];
+
